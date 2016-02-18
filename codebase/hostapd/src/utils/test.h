9aaaa20025dab9fe1f1b92c28de7d814606c9041
root <root@node1-1.sb1.orbit-lab.org>
2016-02-18 13:18:37 -0500

    adding hostapd folder

---
+++ codebase/hostapd/src/utils/test.h
@@ -0,0 +1,9 @@
+#include "eloop.h"
+#include "utils/includes.h"
+#include "stddef.h"
+#include "ap/hostapd.h"
+
+void test_eloop_ctx(void) {
+	printf("Pavan edit: test.h: a simple call test\n");
+	//ToDo: Use eloop_ctx to recieve hostapd_data data structure and obtain parameters.
+}
